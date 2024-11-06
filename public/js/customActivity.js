a36aed9dad2c13dd326c0ab52ae0c6ceea76a718
Shyam <shyam@Admins-MacBook-Pro.local>
2024-11-06 23:27:35 +0530

    new config.json

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,32 +1,58 @@
-let connection = new Postmonger.Session();
-let payload = {};
+var connection = new Postmonger.Session();
+var payload = {};
 
-$(document).ready(function() {
+$(window).ready(onRender);
+
+connection.on('initActivity', initialize);
+connection.on('clickedNext', save);
+
+function onRender() {
+    // Notify Journey Builder that this activity is ready
     connection.trigger('ready');
-    connection.on('initActivity', initialize);
-    connection.on('clickedNext', save);
-});
+}
 
 function initialize(data) {
     if (data) {
         payload = data;
     }
-    
-    // Set the activity as configured immediately
-    payload['metaData'].isConfigured = true;
-    connection.trigger('updateActivity', payload);
+
+    // Request the Entry Source schema
+    connection.trigger('requestSchema');
+    connection.on('requestedSchema', function (data) {
+        // Add entry source attributes as inArgs
+        const schema = data['schema'];
+
+        if (!payload['arguments']) {
+            payload['arguments'] = {};
+        }
+
+        if (!payload['arguments'].execute) {
+            payload['arguments'].execute = {};
+        }
+
+        if (!payload['arguments'].execute.inArguments) {
+            payload['arguments'].execute.inArguments = [];
+        }
+
+        // Add default inArguments
+        payload['arguments'].execute.inArguments = [{
+            "email": "{{Contact.Attribute.manihas_custom_activity.Email}}",
+            "name": "{{Contact.Attribute.manihas_custom_activity.Name}}",
+            "contactKey": "{{Contact.Key}}"
+        }];
+
+        // Add schema fields as inArguments
+        for (var i = 0, l = schema.length; i < l; i++) {
+            var inArg = {};
+            let attr = schema[i].key;
+            let keyIndex = attr.lastIndexOf('.') + 1;
+            inArg[attr.substring(keyIndex)] = '{{' + attr + '}}';
+            payload['arguments'].execute.inArguments.push(inArg);
+        }
+    });
 }
 
 function save() {
-    // Pre-configure the payload with default values
-    payload['arguments'].execute.inArguments = [{
-        "email": "{{Contact.Attribute.manihas_custom_activity.Email}}",
-        "name": "{{Contact.Attribute.manihas_custom_activity.Name}}",
-        "contactKey": "{{Contact.Key}}"
-    }];
-
     payload['metaData'].isConfigured = true;
-
-    console.log('Saving payload:', payload);
     connection.trigger('updateActivity', payload);
 } 
